9e4f475d0317895891d02bcebb125e3f2d000605
Bruno Garcia <mail.brunogarcia@gmail.com>
2021-01-04 06:39:54 -0300

    Add vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import router from './router';
 import App from './App.vue';
 
 import './assets/css/tailwind.css';
@@ -7,5 +8,6 @@ import './assets/css/tailwind.css';
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
